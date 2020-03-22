d982cb5d37fa3fc054d1209742c8125f04563dd0
Markus Pettersson <markus.pettersson1998@gmail.com>
2020-03-22 03:30:29 +0100

    Refactor prefer nesting over threading

--- src/hibp/core.clj
+++ src/hibp/core.clj
@@ -12,10 +12,10 @@
 ;; Create a mapping between a password, it's hash and a shortened hash
 (defn do-the-hash [password]
   (let [hashed-password (str/upper-case (digest/sha1 password))
-        short-hashed-password (subs hashed-password 0 5)]
+        shortened-hash (subs hashed-password 0 5)]
     {:password   password
      :hash       hashed-password
-     :short-hash short-hashed-password}))
+     :short-hash shortened-hash}))
 
 ;; DONE: Make request against api for every password
 (def api-url "https://api.pwnedpasswords.com/range/")
@@ -25,11 +25,10 @@
         request-url (str api-url short-hash)]
     (assoc password
       :pwned
-      (->> request-url
-           (read-lines)
-           (map #(str short-hash %))
-           (map #(str/split % #":"))
-           (into {})))))
+      (into {}
+            (map #(str/split % #":")
+                 (map #(str short-hash %)
+                      (read-lines request-url)))))))
 
 ;; Do some formatting on the result
 (defn pwned? [{hash :hash pwned :pwned password :password}]
@@ -43,8 +42,5 @@
   ;; Check which passwords have been leaked on the web
   (dorun
     (for [password passwords]
-      (if-let [result (->> password
-                           (do-the-hash)
-                           (fetch-pwned)
-                           (pwned?))]
-        (println result)))))
+      (if-let [pwned-password (pwned? (fetch-pwned (do-the-hash password)))]
+        (println pwned-password)))))
